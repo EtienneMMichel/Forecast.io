b70598a7642dbcce5d07412978a133ae12c74e5b
Etienne <etienne.micheladj@gmail.com>
2024-05-26 13:57:43 +0200

    feat(web_app): add bootstrap handler library for dash and use it to build archi and beautiful sidebar :)

--- web_app/app.py
+++ web_app/app.py
@@ -1,8 +1,40 @@
-from dash import Dash, html
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
 
-app = Dash()
+from pages.utils import SIDEBAR_STYLE
 
-app.layout = [html.Div(children='Hello World')]
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages=True)
+
+
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+
+
+
+
+# content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+sidebar = html.Div(
+    [
+        html.A(html.H2("Sidebar", className="display-4"), href="/", style={"text-decoration": "none"}),
+        html.Hr(),
+        html.P(
+            "A simple sidebar layout with navigation links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink(f"{page['name']}", href=page["relative_path"], active="exact")
+            for page in list(filter(lambda page: page['path'] != "/", list(dash.page_registry.values())))],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+app.layout = html.Div([ dash.page_container,sidebar])
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ web_app/pages/utils.py
@@ -0,0 +1,16 @@
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
